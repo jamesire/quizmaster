ec8d5cdb01d699376cc6121618845e0e866f0749
unknown <j.j.reidy@gmail.com>
2020-06-26 00:09:54 +0100

    Using theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,35 @@
-import { CoursesService } from './courses.service';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { CoursesComponent } from './courses.component';
-import { CourseComponent } from './course/course.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AppRoutingModule } from './app.routing';
+import { ComponentsModule } from './components/components.module';
+
 
 @NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    ComponentsModule,
+    NgbModule,
+    RouterModule,
+    AppRoutingModule
+  ],
   declarations: [
     AppComponent,
-    CoursesComponent,
-    CourseComponent
+    AdminLayoutComponent,
+    AuthLayoutComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [ CoursesService ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
