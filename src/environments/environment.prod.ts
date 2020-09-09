553d586029471c9c1f881b46838216fccb86ba18
unknown <j.j.reidy@gmail.com>
2020-09-09 21:21:41 +0100

    Preparation for Heroku push

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  QUIZMASTER_API_URL: "http://quizmasterapi-dev.eu-west-1.elasticbeanstalk.com/",
+  SOCKET_IO_URL: "http://ec2-54-217-57-188.eu-west-1.compute.amazonaws.com:5000/"
 };
